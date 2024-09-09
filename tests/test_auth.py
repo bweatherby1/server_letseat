e372c0c7c354b6f949a8fc46913dfbda1ed9d330
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-09-09 17:27:55 -0500

    added user_name, bio, address, and pictures to all user related files and adjusted tests, all passing and postman calls working as well

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,7 +8,14 @@ class AuthViewsTest(APITestCase):
         self.user = User.objects.create(
             name='Test User',
             uid='testuid123',
-            password=make_password('testpassword')
+            password=make_password('testpassword'),
+            user_name='testuser',
+            bio='Test bio',
+            profile_picture='https://example.com/test.jpg',
+            street_address='123 Test St',
+            city='Test City',
+            state='TS',
+            zip_code='12345'
         )
 
     def test_check_user_valid(self):
@@ -20,11 +27,6 @@ class AuthViewsTest(APITestCase):
         self.assertEqual(response.status_code, 400)
         self.assertFalse(response.data['valid'])
 
-
-
-
-
-
     def test_check_user_invalid(self):
         data = {
             'uid': 'testuid123',
@@ -34,20 +36,39 @@ class AuthViewsTest(APITestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, {'valid': False})
 
-
-
     def test_register_user(self):
         data = {
             'name': 'New User',
             'uid': 'newuid456',
-            'password': 'newpassword'
+            'password': 'newpassword',
+            'user_name': 'newuser',
+            'bio': 'New user bio',
+            'profile_picture': 'https://example.com/new.jpg',
+            'street_address': '456 New St',
+            'city': 'New City',
+            'state': 'NS',
+            'zip_code': '67890'
         }
         response = self.client.post('/register', data)
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(response.data['id'])
         self.assertEqual(response.data['uid'], 'newuid456')
         self.assertEqual(response.data['name'], 'New User')
+        self.assertEqual(response.data['user_name'], 'newuser')
+        self.assertEqual(response.data['bio'], 'New user bio')
+        self.assertEqual(response.data['profile_picture'], 'https://example.com/new.jpg')
+        self.assertEqual(response.data['street_address'], '456 New St')
+        self.assertEqual(response.data['city'], 'New City')
+        self.assertEqual(response.data['state'], 'NS')
+        self.assertEqual(response.data['zip_code'], '67890')
 
         new_user = User.objects.get(uid='newuid456')
         self.assertIsNotNone(new_user)
         self.assertEqual(new_user.name, 'New User')
+        self.assertEqual(new_user.user_name, 'newuser')
+        self.assertEqual(new_user.bio, 'New user bio')
+        self.assertEqual(new_user.profile_picture, 'https://example.com/new.jpg')
+        self.assertEqual(new_user.street_address, '456 New St')
+        self.assertEqual(new_user.city, 'New City')
+        self.assertEqual(new_user.state, 'NS')
+        self.assertEqual(new_user.zip_code, '67890')
