cfcba4e61cc07d8c8c3775bf7024581273457f78
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2025-01-12 20:35:15 -0600

    messing with cors path

--- letseat/settings.py
+++ letseat/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://client-letseat-production.up.railway.app'
+    'https://clientletseat-production.up.railway.app'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
